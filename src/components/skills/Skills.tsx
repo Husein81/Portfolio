7561a91b477c482bb414ba4aee6950a0713c25ba
Hussein Nasrallah <109015272+Husein81@users.noreply.github.com>
2024-07-23 12:44:37 +0300

    .

--- src/components/skills/Skills.tsx
+++ src/components/skills/Skills.tsx
@@ -7,15 +7,15 @@ const Skills = () => {
     const skills: Skill[] = [
         {name:'Asp.Net', imageUrl: '../../../public/assets/skills/ASP.png'},
         {name:'React', imageUrl: '../../../public/assets/skills/react.png'},
-        {name:'React Native', imageUrl: '../../../public/assets/skills/reactNative.jpg'},
+        {name:'React Native', imageUrl: '../../../public/assets/skills/reactNative.png'},
         {name:'TypeScript', imageUrl: '../../../public/assets/skills/TS.png'},
         {name:"Tailwind CSS", imageUrl: '../../../public/assets/skills/tailwind.png'},
         {name:'Java', imageUrl: '../../../public/assets/skills/Java.png'},
-        {name:"Kotlin", imageUrl:'../../../public/assets/skills/kotlin.jpg'},
+        {name:"Kotlin", imageUrl:'../../../public/assets/skills/kotlin.png'},
         {name:'Node', imageUrl: '../../../public/assets/skills/node.png'},
         {name:'MongoDB', imageUrl: '../../../public/assets/skills/MongoDB.png'},
         {name:'MySQL', imageUrl: '../../../public/assets/skills/mysSQL.png'},
-        {name:"Git and Github", imageUrl: '../../../public/assets/skills/git.jpg'},
+        {name:"Git and Github", imageUrl: '../../../public/assets/skills/git.png'},
         
     ]
   return (
